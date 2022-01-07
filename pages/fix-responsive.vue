da4eaf94930ec393c64810b9a4feb83f65906f46
Adrian Lozovanu <adrian_lozovanu@yahoo.com>
2022-01-07 14:32:21 +0100

    fix: make the webpage responsive with css only

--- pages/fix-responsive.vue
+++ pages/fix-responsive.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="page-wrap">
-
     <h1>Make This Responsive</h1>
 
     <p>While maintaining the hierarchy of importance.</p>
 
     <article class="main-story">
-
-      <img src="/she-ra.jpg" alt="Sha Ra Rocking"/>
+      <img src="/she-ra.jpg" alt="Sha Ra Rocking" />
 
       <div class="story-intro">
         <h1>Most Important Story</h1>
-        <p>This article has the most visual weight. <a href="http://nebezial.deviantart.com/art/she-ra-115867096">image source.</a></p>
+        <p>
+          This article has the most visual weight.
+          <a href="http://nebezial.deviantart.com/art/she-ra-115867096"
+            >image source.</a
+          >
+        </p>
       </div>
-
     </article>
 
     <section class="sub-stories">
+      <article class="sub-story">
+        <img src="/kitten.jpg" />
+        <div class="story-intro">
+          <h2>Less Important Story</h2>
+          <p>This story has less visual weight.</p>
+        </div>
+      </article>
 
       <article class="sub-story">
-        <img src="/kitten.jpg"/>
+        <img src="/nick-cage.jpg" />
         <div class="story-intro">
           <h2>Less Important Story</h2>
           <p>This story has less visual weight.</p>
@@ -27,49 +36,43 @@
       </article>
 
       <article class="sub-story">
-        <img src="/nick-cage.jpg"/>
+        <img src="/bears.jpg" />
         <div class="story-intro">
           <h2>Less Important Story</h2>
           <p>This story has less visual weight.</p>
         </div>
       </article>
-
-      <article class="sub-story last">
-        <img src="/bears.jpg"/>
-        <div class="story-intro">
-          <h2>Less Important Story</h2>
-          <p>This story has less visual weight.</p>
-        </div>
-      </article>
-
     </section>
-
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
 
 <style scoped>
 * {
   box-sizing: border-box;
 }
-
 .page-wrap {
-  width: 800px;
-  margin: 20px auto;
+  --size: 20px;
+  width: 100%;
+  max-width: 800px;
+  margin: auto;
+  padding: var(--size);
 }
 
 .main-story {
   position: relative;
-  margin: 0 0 25px 0;
+  margin: 0 0 var(--size) 0;
 }
 
 img {
   display: block;
+  object-fit: cover;
+  width: 100%;
 }
 
 a {
@@ -82,15 +85,16 @@ a {
   left: 0;
   width: 100%;
   background: rgba(0, 0, 0, 0.75);
-  padding: 20px;
+  padding: var(--size);
   color: white;
 }
 
 h1 {
-   font-size: 4em;
+  font-size: 4em;
 }
-h1, h2 {
-  margin: 0 0 10px 0;
+h1,
+h2 {
+  margin: 0 0 var(--size) 0;
 }
 
 .story-intro h1 {
@@ -101,19 +105,31 @@ h1, h2 {
 }
 
 .sub-stories {
-  overflow: hidden;
-  margin: 0 0 25px 0;
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--size);
 }
 
 .sub-story {
-  float: left;
-  width: 250px;
-  margin-right: 25px;
   position: relative;
+  /* We make the display: flex so that we can center the image inside */
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-size: 80%;
+  flex: 1 0 0px;
+  min-width: 150px;
+  /* We roughly make the height of the sub-stories equal to the height of the
+   * main story, and then make it 2/3 to respect the Importance Hierarchy */
+  max-height: calc(((100vw - var(--size) * 2) * 9 / 16) * 2 / 3);
+  overflow: hidden;
 }
 
-.last {
-  margin-right: 0;
+/* Change the font size for smaller screens */
+@media screen and (max-width: 450px) {
+  .page-wrap {
+    font-size: 14px;
+    --size: 10px;
+  }
 }
 </style>
